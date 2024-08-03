e508a540e5feaef93702b88327613d8d59a905e0
bishoy-girgis <girgisbishoy6@gmail.com>
2024-08-03 12:32:16 +0200

    refactor SHIMMER VACATION

--- lib/Features/vacations/pages/vacations_view.dart
+++ lib/Features/vacations/pages/vacations_view.dart
@@ -1,6 +1,7 @@
 import 'package:easy_hr/Domain/entity/vacation/vacation_data_entity.dart';
 import 'package:easy_hr/Features/vacations/manager/vacation_cubit.dart';
 import 'package:easy_hr/Features/vacations/manager/vacation_states.dart';
+import 'package:easy_hr/Features/vacations/widgets/vacation_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,6 +15,7 @@ import '../../../Core/widgets/custom_text_form_field.dart';
 import '../../../Core/widgets/gap.dart';
 import '../../../Core/widgets/text_builder.dart';
 import '../../../main.dart';
+import '../../profileInfo/widgets/profile_shimmer_widget.dart';
 
 class VacationsView extends StatefulWidget {
   const VacationsView({super.key});
@@ -60,138 +62,46 @@ class _VacationsViewState extends State<VacationsView> {
               child: Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(16),
-                  color: Colors.white,
+                  color: AppColors.whiteColor,
                 ),
                 child: BlocProvider<VacationCubit>(
                   create: (context) => VacationCubit()..getVacations(),
                   child: BlocConsumer<VacationCubit, VacationState>(
-                      listener: (context, state) {},
-                      builder: (context, state) {
-                        var cubit = VacationCubit.get(context);
-                        vacations = cubit.vacations;
+                    listener: (context, state) {},
+                    builder: (context, state) {
+                      var cubit = VacationCubit.get(context);
+                      vacations = cubit.vacations;
+                      if (state is VacationSuccessState) {
                         return ListView.builder(
-                          // reverse: true,
                           physics: const BouncingScrollPhysics(),
                           padding: const EdgeInsets.symmetric(
                               vertical: 10, horizontal: 10),
                           itemCount: vacations.length,
                           itemBuilder: (context, index) {
                             var vacation = vacations[index];
-                            return InkWell(
-                              onTap: () {},
-                              child: Container(
-                                padding: EdgeInsets.symmetric(
-                                    vertical: 10.h, horizontal: 12.w),
-                                margin: EdgeInsets.symmetric(
-                                    horizontal: 10.w, vertical: 6.h),
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(18),
-                                  color: Colors.white,
-                                  boxShadow: const [
-                                    BoxShadow(
-                                      color: Colors.black45,
-                                      spreadRadius: 1,
-                                      blurRadius: 4,
-                                      offset: Offset(1, 1),
-                                    ),
-                                  ],
-                                ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        ImageIcon(
-                                          const AssetImage(
-                                              "assets/images/beach.png"),
-                                          color: AppColors.primaryColorGrey,
-                                          size: 24.sp,
-                                        ),
-                                        const GapW(w: 4),
-                                        TextBuilder(
-                                            vacation.vacationNumber ?? "")
-                                      ],
-                                    ),
-                                    const GapH(h: 1),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        const TextBuilder("Vacation request"),
-                                        const GapW(w: 4),
-                                        TextBuilder(
-                                          AppConstants.formatDate(
-                                              vacation.vacationDate ??
-                                                  "1990-10-01T00:00:00"),
-                                        )
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [
-                                        const GapW(w: 4),
-                                        TextBuilder(
-                                            vacation.approved
-                                                ? "Approved"
-                                                : "in review",
-                                            color: vacation.approved
-                                                ? Colors.green.withOpacity(0.7)
-                                                : Colors.black
-                                                    .withOpacity(0.7)),
-                                      ],
-                                    ),
-                                    const GapH(h: 2),
-                                    Row(
-                                      children: [
-                                        TextBuilder("vacation type",
-                                            color:
-                                                Colors.black.withOpacity(0.5)),
-                                        const GapW(w: 22),
-                                        const TextBuilder(" : year")
-                                      ],
-                                    ),
-                                    const GapH(h: 1),
-                                    Row(
-                                      children: [
-                                        TextBuilder("Days count",
-                                            color:
-                                                Colors.black.withOpacity(0.5)),
-                                        const GapW(w: 27),
-                                        TextBuilder(": ${vacation.days}"),
-                                        const GapW(w: 1),
-                                        TextBuilder("Day/s",
-                                            color:
-                                                Colors.black.withOpacity(0.5)),
-                                      ],
-                                    ),
-                                    const GapH(h: 1),
-                                    Row(
-                                      children: [
-                                        TextBuilder("From   ",
-                                            color:
-                                                Colors.black.withOpacity(0.5)),
-                                        TextBuilder(
-                                          AppConstants.formatDate(
-                                              vacation.dateFrom ??
-                                                  "1990-10-01T00:00:00"),
-                                        ),
-                                        TextBuilder("   To   ",
-                                            color:
-                                                Colors.black.withOpacity(0.5)),
-                                        TextBuilder(
-                                          AppConstants.formatDate(
-                                              vacation.dateTo ??
-                                                  "1990-10-01T00:00:00"),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            );
+                            return VacationItem(vacation: vacation);
                           },
                         );
-                      }),
+                      } else if (state is VacationErrorState) {
+                        debugPrint(state.failure.message);
+                        return Center(
+                          child: TextBuilder("Sorry ${state.failure.message}"),
+                        );
+                      } else if (state is VacationLoadingState) {
+                        return ListView.builder(
+                          physics: const BouncingScrollPhysics(),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 10),
+                          itemCount: 10,
+                          itemBuilder: (context, index) {
+                            return const ProfileShimmerWidget();
+                          },
+                        );
+                      } else {
+                        return const CircularProgressIndicator();
+                      }
+                    },
+                  ),
                 ),
               ),
             )
---
+++ lib/Features/vacations/widgets/vacation_item.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../Core/constants/app_colors.dart';
+import '../../../Core/constants/app_constants.dart';
+import '../../../Core/widgets/gap.dart';
+import '../../../Core/widgets/text_builder.dart';
+import '../../../Domain/entity/vacation/vacation_data_entity.dart';
+
+class VacationItem extends StatelessWidget {
+  VacationDataEntity vacation ;
+   VacationItem({super.key,required this.vacation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+          vertical: 10.h, horizontal: 12.w),
+      margin: EdgeInsets.symmetric(
+          horizontal: 10.w, vertical: 6.h),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(18),
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black45,
+            spreadRadius: 1,
+            blurRadius: 4,
+            offset: Offset(1, 1),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              ImageIcon(
+                const AssetImage(
+                    "assets/images/beach.png"),
+                color: AppColors.primaryColorGrey,
+                size: 24.sp,
+              ),
+              const GapW(w: 4),
+              TextBuilder(
+                  vacation.vacationNumber ?? "")
+            ],
+          ),
+          const GapH(h: 1),
+          Row(
+            mainAxisAlignment:
+            MainAxisAlignment.spaceBetween,
+            children: [
+              const TextBuilder("Vacation request"),
+              const GapW(w: 4),
+              TextBuilder(
+                AppConstants.formatDate(
+                    vacation.vacationDate ??
+                        "1990-10-01T00:00:00"),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              const GapW(w: 4),
+              TextBuilder(
+                  vacation.approved
+                      ? "Approved"
+                      : "in review",
+                  color: vacation.approved
+                      ? Colors.green.withOpacity(0.7)
+                      : Colors.black
+                      .withOpacity(0.7)),
+            ],
+          ),
+          const GapH(h: 2),
+          Row(
+            children: [
+              TextBuilder("vacation type",
+                  color:
+                  Colors.black.withOpacity(0.5)),
+              const GapW(w: 22),
+              const TextBuilder(" : year")
+            ],
+          ),
+          const GapH(h: 1),
+          Row(
+            children: [
+              TextBuilder("Days count",
+                  color:
+                  Colors.black.withOpacity(0.5)),
+              const GapW(w: 27),
+              TextBuilder(": ${vacation.days}"),
+              const GapW(w: 1),
+              TextBuilder("Day/s",
+                  color:
+                  Colors.black.withOpacity(0.5)),
+            ],
+          ),
+          const GapH(h: 1),
+          Row(
+            children: [
+              TextBuilder("From   ",
+                  color:
+                  Colors.black.withOpacity(0.5)),
+              TextBuilder(
+                AppConstants.formatDate(
+                    vacation.dateFrom ??
+                        "1990-10-01T00:00:00"),
+              ),
+              TextBuilder("   To   ",
+                  color:
+                  Colors.black.withOpacity(0.5)),
+              TextBuilder(
+                AppConstants.formatDate(
+                    vacation.dateTo ??
+                        "1990-10-01T00:00:00"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
