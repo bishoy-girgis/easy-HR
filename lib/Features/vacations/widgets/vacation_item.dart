accf88ce73d89f86f6d55268d5ffbc763ed46fcd
bishoy-girgis <girgisbishoy6@gmail.com>
2024-09-28 16:39:11 +0200

    refactor code

--- lib/Features/vacations/widgets/vacation_item.dart
+++ lib/Features/vacations/widgets/vacation_item.dart
@@ -8,17 +8,19 @@ import '../../../Core/widgets/text_builder.dart';
 import '../../../Domain/entity/vacation/vacation_data_entity.dart';
 
 class VacationItem extends StatelessWidget {
-  VacationDataEntity vacation ;
-   VacationItem({super.key,required this.vacation});
+  VacationDataEntity vacation;
+
+  VacationItem({
+    required this.vacation,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     var local = AppLocalizations.of(context)!;
     return Container(
-      padding: EdgeInsets.symmetric(
-          vertical: 10.h, horizontal: 12.w),
-      margin: EdgeInsets.symmetric(
-          horizontal: 10.w, vertical: 6.h),
+      padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 12.w),
+      margin: EdgeInsets.symmetric(horizontal: 10.w, vertical: 6.h),
       width: double.infinity,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(18),
@@ -38,27 +40,23 @@ class VacationItem extends StatelessWidget {
           Row(
             children: [
               ImageIcon(
-                const AssetImage(
-                    "assets/images/beach.png"),
+                const AssetImage("assets/images/beach.png"),
                 color: AppColors.primaryColorGrey,
                 size: 24.sp,
               ),
               const GapW(w: 4),
-              TextBuilder(
-                  vacation.vacationNumber ?? "")
+              TextBuilder(vacation.vacationNumber ?? "")
             ],
           ),
           const GapH(h: 1),
           Row(
-            mainAxisAlignment:
-            MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-               TextBuilder(local.vacationRequest),
+              TextBuilder(local.vacationRequest),
               const GapW(w: 4),
               TextBuilder(
                 AppConstants.formatDate(
-                    vacation.vacationDate ??
-                        "1990-10-01T00:00:00"),
+                    vacation.vacationDate ?? "1990-10-01T00:00:00"),
               )
             ],
           ),
@@ -66,13 +64,11 @@ class VacationItem extends StatelessWidget {
             children: [
               const GapW(w: 4),
               TextBuilder(
-                  vacation.approved
-                      ? local.approved
-                      : local.inReview,
-                  color: vacation.approved
-                      ? Colors.green[900]
-                      : Colors.black
-                      .withOpacity(0.7)),
+                vacation.approved ? local.approved : local.inReview,
+                color: vacation.approved
+                    ? Colors.green[900]
+                    : Colors.black.withOpacity(0.7),
+              ),
             ],
           ),
           const GapH(h: 2),
@@ -80,11 +76,13 @@ class VacationItem extends StatelessWidget {
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.4,
-                child: TextBuilder(local.vacationType,
-                    color:
-                    Colors.black.withOpacity(0.5),),
+                child: TextBuilder(
+                  local.vacationType,
+                  color: Colors.black.withOpacity(0.5),
+                ),
               ),
-               TextBuilder(": ${vacation.vacationName ??vacation.vacationEName }")
+              TextBuilder(
+                  ": ${vacation.vacationName ?? vacation.vacationEName}")
             ],
           ),
           const GapH(h: 1),
@@ -92,16 +90,17 @@ class VacationItem extends StatelessWidget {
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.4,
-
-                child: TextBuilder(local.daysCount,
-                    color:
-                    Colors.black.withOpacity(0.5)),
+                child: TextBuilder(
+                  local.daysCount,
+                  color: Colors.black.withOpacity(0.5),
+                ),
               ),
               TextBuilder(": ${vacation.days}"),
               const GapW(w: 1),
-              TextBuilder(local.dayss,
-                  color:
-                  Colors.black.withOpacity(0.5)),
+              TextBuilder(
+                local.dayss,
+                color: Colors.black.withOpacity(0.5),
+              ),
             ],
           ),
           const GapH(h: 1),
@@ -111,15 +110,14 @@ class VacationItem extends StatelessWidget {
                 children: [
                   SizedBox(
                     width: MediaQuery.of(context).size.width * 0.1,
-
-                    child: TextBuilder(local.from,
-                        color:
-                        Colors.black.withOpacity(0.5)),
+                    child: TextBuilder(
+                      local.from,
+                      color: Colors.black.withOpacity(0.5),
+                    ),
                   ),
                   TextBuilder(
                     AppConstants.formatDate(
-                        vacation.dateFrom ??
-                            "1990-10-01T00:00:00"),
+                        vacation.dateFrom ?? "1990-10-01T00:00:00"),
                   ),
                 ],
               ),
@@ -128,19 +126,17 @@ class VacationItem extends StatelessWidget {
                 children: [
                   SizedBox(
                     width: MediaQuery.of(context).size.width * 0.1,
-
-                    child: TextBuilder(local.to,
-                        color:
-                        Colors.black.withOpacity(0.5)),
+                    child: TextBuilder(
+                      local.to,
+                      color: Colors.black.withOpacity(0.5),
+                    ),
                   ),
                   TextBuilder(
                     AppConstants.formatDate(
-                        vacation.dateTo ??
-                            "1990-10-01T00:00:00"),
+                        vacation.dateTo ?? "1990-10-01T00:00:00"),
                   ),
                 ],
               ),
-
             ],
           ),
         ],
